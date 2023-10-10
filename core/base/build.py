281d47385fbb8f931589cb1a65fb47766edcd7a6
Carl <carlmichelle493@gmail.com>
2023-10-10 18:42:52 +0800

    优化

---
+++ core/base/build.py
@@ -0,0 +1,22 @@
+# 构建参数方法
+
+# 任务参数设置  将点击延迟和随机点击迁移到点击事件名中
+# 整体参数结构设计
+# 匹配规则，ocr匹配:需要有匹配的文字参数 模板匹配:需要模板图片名字
+# 点击事件名，点击事件
+class TaskFlow:
+    self.nextTask:TaskFlows
+    
+    class Tasks:
+    def __init__(self,head):
+        """
+        head 任务入口 必须
+        """
+        self.head = head
+    
+    def setNextTask:
+        
+    
+class Build:
+   # 数据结构有向无环图
+   # 首节点 模板参数 k：
